dfb46ca1c75b3799384b05c0ea5dd25a71017540
Stephan <github@athome.zone>
2020-08-23 14:48:55 +0200

    More dynamical forward

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -3,7 +3,10 @@ echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER
 
 sleep 5
 
-iptables -t nat -A PREROUTING -i tun0 -p tcp --dport 803389 -j DNAT --to 10.0.2.51:3389
+iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $RDPPort -j DNAT --to 10.0.2.51:3389
+if [[ ! -z "$LasernetPort" ]]; then
+  iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $LasernetPort -j DNAT --to 10.0.2.51:3479
+fi
 iptables -t nat -A POSTROUTING -o tun0 -p tcp -d 10.0.2.51 --dport 3389 -j MASQUERADE
 
 iptables -A FORWARD -i eth1 -j ACCEPT
