2db2094febb229488791c19dd7cc7a80f42a183e
Stephan <github@athome.zone>
2020-08-23 15:15:11 +0200

    added some logs

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --no-cert-check -b
+
+printf "Build VPN connection to" "$ANYCONNECT_SERVER"
+printf "Build VPN connection with user" "$ANYCONNECT_USER"
+printf "Build VPN connection with Servercert" "$ServerCert"
+
+
+echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert $ServerCert
 
 sleep 5
 
 iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $RDPPort -j DNAT --to $RDPGateway:3389
 iptables -t nat -A POSTROUTING -o tun0 -p tcp -d $RDPGateway --dport 3389 -j MASQUERADE
+printf "Build RDP Forward to Server" "$RDPGateway"
+printf "Build RDP Forward and publish this via port" "$RDPPort"
 if [[ ! -z "$LasernetPort" ]]; then
   iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $LasernetPort -j DNAT --to $RDPGateway:3479
   iptables -t nat -A POSTROUTING -o tun0 -p tcp -d $RDPGateway --dport 3479 -j MASQUERADE
+  printf "Build Lasernet Forward and publish this via port" "$LasernetPort"
 fi
 
 
