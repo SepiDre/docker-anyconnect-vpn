2edec90965674f2206550450d3d3423455009f4f
Stephan <github@athome.zone>
2020-08-24 09:00:50 +0200

    replaced printf with echo

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-printf "Build VPN connection to" "$ANYCONNECT_SERVER"
-printf "Build VPN connection with user" "$ANYCONNECT_USER"
-printf "Build VPN connection with Servercert" "$ServerCert"
+echo "Build VPN connection to" "$ANYCONNECT_SERVER"
+echo "Build VPN connection with user" "$ANYCONNECT_USER"
+echo "Build VPN connection with Servercert" "$ServerCert"
 
 
 echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert $ServerCert
@@ -12,12 +12,12 @@ sysctl -w net.ipv4.ip_forward=1
 
 iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $RDPPort -j DNAT --to $RDPGateway:3389
 iptables -t nat -A POSTROUTING -o tun0 -p tcp -d $RDPGateway --dport 3389 -j MASQUERADE
-printf "Build RDP Forward to Server" "$RDPGateway"
-printf "Build RDP Forward and publish this via port" "$RDPPort"
+echo "Build RDP Forward to Server" "$RDPGateway"
+echo "Build RDP Forward and publish this via port" "$RDPPort"
 if [[ ! -z "$LasernetPort" ]]; then
   iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $LasernetPort -j DNAT --to $RDPGateway:3479
   iptables -t nat -A POSTROUTING -o tun0 -p tcp -d $RDPGateway --dport 3479 -j MASQUERADE
-  printf "Build Lasernet Forward and publish this via port" "$LasernetPort"
+  echo "Build Lasernet Forward and publish this via port" "$LasernetPort"
 fi
 
 
