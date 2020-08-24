a0c79628054aece378f3f3f689b77e49e07e4b10
Stephan <github@athome.zone>
2020-08-24 10:44:47 +0200

    changed to nft

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -8,19 +8,23 @@ echo "Build VPN connection with Servercert" "$ServerCert"
 echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert $ServerCert
 
 sleep 5
-sysctl -w net.ipv4.ip_forward=1
+nft -f /etc/nftables/ipv4-nat;
 
-iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $RDPPort -j DNAT --to $RDPGateway:3389
-iptables -t nat -A POSTROUTING -o tun0 -p tcp -d $RDPGateway --dport 3389 -j MASQUERADE
+nft add table nat;
+nft add chain nat post { type nat hook postrouting priority 0 \; }
+nft add chain nat pre { type nat hook prerouting priority 0 \; }
+nft add rule nat postrouting masquerade random;
+
+nft add rule nat post iif eth0 oif tun0 snat $RDPGateway;
+nft add rule nat pre tcp dport 3389 iif eth0 dnat $RDPGateway:3389;
 echo "Build RDP Forward to Server" "$RDPGateway"
 echo "Build RDP Forward and publish this via port" "$RDPPort"
-if [[ ! -z "$LasernetPort" ]]; then
-  iptables -t nat -A PREROUTING -i tun0 -p tcp --dport $LasernetPort -j DNAT --to $RDPGateway:3479
-  iptables -t nat -A POSTROUTING -o tun0 -p tcp -d $RDPGateway --dport 3479 -j MASQUERADE
+if [[ ! -z "$LasernetPort" ]]; then 
+  nft add rule nat pre tcp dport $LasernetPort iif eth0 dnat $RDPGateway:3479;
   echo "Build Lasernet Forward and publish this via port" "$LasernetPort"
 fi
 
-
+nft add rule filter iif eth0 oif tun0 accept
 iptables -A FORWARD -i eth1 -j ACCEPT
 
 route del -net 172.17.0.0 netmask 255.255.240.0  dev tun0
